d7e619af552e08accde14eff17ad60d9ce38e142
magnabosco <118080289+NicolasMagnabosco@users.noreply.github.com>
2024-07-08 12:32:47 -0300

    something

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import dotenv from "dotenv";
 import cursosRouter from "./routes/cursos.router.js";
 import reseñasRouter from "./routes/reseñas.router.js";
 import authRouter from "./routes/auth.router.js";
-// dotenv.config();
+dotenv.config();
 export const app = express();
 
-// const PORT = 3000;
-
-// app.get("/", (req, res) => {
-//   res.send("Hey!");
-// });
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.use(express.json());
 app.use("/cursos", cursosRouter);
