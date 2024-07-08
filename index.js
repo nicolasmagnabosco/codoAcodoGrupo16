af2fcc612dda99c4e91cb16f19678388f5100e88
magnabosco <118080289+NicolasMagnabosco@users.noreply.github.com>
2024-07-08 13:18:47 -0300

    attempt

--- index.js
+++ index.js
@@ -4,9 +4,15 @@ import cursosRouter from "./api/routes/cursos.router.js";
 import reseñasRouter from "./api/routes/reseñas.router.js";
 import authRouter from "./api/routes/auth.router.js";
 dotenv.config();
-export const app = express();
+const app = express();
 
 app.use(express.json());
 app.use("/cursos", cursosRouter);
 app.use("/resenas", reseñasRouter);
 app.use("/auth", authRouter);
+
+app.listen(3000, () => {
+  console.log("Hey");
+});
+
+export default app;
